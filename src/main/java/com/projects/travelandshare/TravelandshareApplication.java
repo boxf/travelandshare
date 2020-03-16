79cbe3a662c2f2951f7c3a1dd0063804dccded9f
boxf <61790360+boxf@users.noreply.github.com>
2020-03-16 14:47:57 +0100

    Initial Commit : Basic structure and dependencies of the project. Tests don't pass.

---
+++ src/main/java/com/projects/travelandshare/TravelandshareApplication.java
@@ -0,0 +1,13 @@
+package com.projects.travelandshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelandshareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelandshareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projects/travelandshare/TravelandshareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.travelandshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelandshareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
