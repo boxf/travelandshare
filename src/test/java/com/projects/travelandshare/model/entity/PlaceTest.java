b6ba7084cb7d33a4ca99a8d3982709dbfa1075bd
MarionPradeau <MarionPradeau@users.github.com>
2020-03-17 17:28:21 +0100

    Après discussion avec Boris la méthode de test de Getter et de Setter devait être différenciés. La classe Field fournit des informations et un accès dynamique à un seul champ d'une classe ou d'une interface.

--- src/test/java/com/projects/travelandshare/model/entity/PlaceTest.java
+++ src/test/java/com/projects/travelandshare/model/entity/PlaceTest.java
@@ -4,17 +4,35 @@ import com.projects.travelandshare.util.Counties;
 import com.projects.travelandshare.util.Types;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Field;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlaceTest {
 
     @Test
-    void testSetName_testGetName() {
-    Place place = new Place();
-    String resultExpected = "Calanques";
-    place.setName("Calanques");
-    assertEquals(resultExpected, place.getName());
-        System.out.println(resultExpected);
+    void testSetName_setsProperly() throws NoSuchFieldException, IllegalAccessException {
+//given
+        final Place place = new Place();
+//when
+        place.setName("Calanques");
+//then
+        final Field field = place.getClass().getDeclaredField("name");
+        field.setAccessible(true);
+        assertEquals("Calanques", field.get(place));
+        System.out.println(field.get(place));
+    }
+    @Test
+    public void testGetName_getsValues() throws NoSuchFieldException, IllegalAccessException {
+//given
+        final Place place = new Place();
+        final Field field = place.getClass().getDeclaredField("name");
+        field.setAccessible(true);
+        field.set(place, "Paris");
+//when
+        final String result = place.getName();
+//then
+        assertEquals("Paris", result);
         System.out.println(place.getName());
     }
 }
