ce879a423875707a737790c2d6468b6fd377e85a
CedricP-Git <61793442+CedricP-Git@users.noreply.github.com>
2020-03-17 11:42:32 +0100

    Created all packages

--- src/main/java/com/projects/travelandshare/service/PlaceService.java
+++ src/main/java/com/projects/travelandshare/service/PlaceService.java
@@ -1,4 +1,14 @@
 package com.projects.travelandshare.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+
 public class PlaceService {
+//@Autowired
+//    private PlaceRepository placeRepository;
+//public void registerPlace (Place place){
+//    this.placeRepository.addPlace();
+//}
+
+
 }
