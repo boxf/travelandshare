e6d2204b21acb38fcf7af0e03a1f12d94fd378ec
F-Dambrine <61804908+F-Dambrine@users.noreply.github.com>
2020-03-17 14:09:33 +0100

    Methode findPlaceByCounty dans placeService qui retourne une liste de place

--- src/main/java/com/projects/travelandshare/service/PlaceService.java
+++ src/main/java/com/projects/travelandshare/service/PlaceService.java
@@ -1,4 +1,22 @@
 package com.projects.travelandshare.service;
 
+import com.projects.travelandshare.model.entity.Place;
+import com.projects.travelandshare.repository.PlaceRepository;
+import com.projects.travelandshare.util.Counties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PlaceService {
+
+    @Autowired
+    private PlaceRepository repository;
+    private Place place;
+
+    public List<Place> findPlaceByCounty(Counties counties){
+        List<Place> placeList = repository.findAllByCounty(counties);
+        return placeList;
+    }
 }
