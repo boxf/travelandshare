b32ed35e5780a43c443ec455b301b4f27644ccac
MarionPradeau <MarionPradeau@users.github.com>
2020-03-17 12:37:23 +0100

    Test pour le setPlace fait. Si le résultat passé dans le setter n'ai pas identique a celui attendu alors le test échoue, ce que indique que le setter prend bien en compte la modification passé en paramètre.

---
+++ src/test/java/com/projects/travelandshare/model/entity/PlaceTest.java
@@ -0,0 +1,20 @@
+package com.projects.travelandshare.model.entity;
+
+import com.projects.travelandshare.util.Counties;
+import com.projects.travelandshare.util.Types;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlaceTest {
+
+    @Test
+    void setName() {
+    Place place = new Place();
+    String resultExpected = "Calanques";
+    place.setName("Calanques");
+    assertEquals(resultExpected, place.getName());
+        System.out.println(resultExpected);
+        System.out.println(place.getName());
+    }
+}
