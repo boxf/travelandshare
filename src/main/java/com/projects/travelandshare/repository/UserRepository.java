410e61f1c055f738d1f2b7b35d1065926ab73f9a
F-Dambrine <61804908+F-Dambrine@users.noreply.github.com>
2020-03-17 10:37:47 +0100

    Created PlaceRepository

---
+++ src/main/java/com/projects/travelandshare/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.projects.travelandshare.repository;
+
+import com.projects.travelandshare.model.entity.Place;
+import com.projects.travelandshare.model.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+
+}
