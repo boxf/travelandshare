89ea84b7eb62a2f2284e610ba206a9f94be78e05
F-Dambrine <61804908+F-Dambrine@users.noreply.github.com>
2020-03-18 10:38:56 +0100

    Annotation JavaDoc for François's method on PlaceRepository, PlaceService, PlaceRestController

--- src/main/java/com/projects/travelandshare/restcontroller/PlaceRestController.java
+++ src/main/java/com/projects/travelandshare/restcontroller/PlaceRestController.java
@@ -12,21 +12,39 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+/**
+ * RestController for Place. It permish to get back the information in the model and communicate with the view
+ */
 @RestController
 @RequestMapping("/api")
 //@CrossOrigin(origins = "http://localhost:4200/")
 public class PlaceRestController {
 
+    /**
+     * Dependencies injection of PlaceService
+     */
     @Autowired
     private PlaceService placeService;
 
 
+    /**
+     * Method for have a list of places by their counties. It works with the url ".../api/place/" + the county
+     * @param counties: enumeration of counties
+     * @return a list of places on a HTTP page
+     * @author Dambrine François
+     */
     @RequestMapping("/place/{counties}")
     List<Place> getPlaceByCounty(@PathVariable("counties") Counties counties){
         List<Place> placeList = placeService.findPlaceByCounty(counties);
        return placeList;
     }
 
+    /**
+     *
+     * Methode for have the all places in the dataBase. It works with the url ".../api/places"
+     * @return a list of all the places on a HTTP page
+     * @author Dambrine François
+     */
     @RequestMapping ("/places")
     public List<Place> getAllPlace() {
         List<Place> placeList = placeService.findAllPlace();
--- src/main/java/com/projects/travelandshare/service/PlaceService.java
+++ src/main/java/com/projects/travelandshare/service/PlaceService.java
@@ -8,17 +8,35 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * A service classe which defines the required method on Place
+ */
 @Service
 public class PlaceService {
 
+    /**
+     * Dependencies injection of PlaceRepository
+     */
     @Autowired
     private PlaceRepository repository;
-    private Place place;
 
+
+    /**
+     * Used to find a list of places thanks to theirs counties in the dataBase
+     * @param counties: enumeration of counties
+     * @return a list of places
+     * @author Dambrine François
+     */
     public List<Place> findPlaceByCounty(Counties counties){
         List<Place> placeList = repository.findAllByCounty(counties);
         return placeList;
     }
+
+    /**
+     * Used to find all the places which are in the dataBase
+     * @return a list of all the places
+     * @author Dambrine François
+     */
     public List<Place> findAllPlace (){
         List<Place> placeList = (List<Place>) repository.findAll();
         return placeList;
