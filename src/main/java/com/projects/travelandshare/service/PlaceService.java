af1582788c99f76204f4c6c55adfc5ecd8a31cc8
CedricP-Git <61793442+CedricP-Git@users.noreply.github.com>
2020-03-19 09:20:52 +0100

    testing the upload image methods

--- src/main/java/com/projects/travelandshare/service/PlaceService.java
+++ src/main/java/com/projects/travelandshare/service/PlaceService.java
@@ -10,16 +10,9 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
-/**
- * A service classe which defines the required method on Place
- */
 
 @Service
 public class PlaceService {
-
-    /**
-     * Dependencies injection of PlaceRepository
-     */
     @Autowired
     private PlaceRepository placeRepository;
     @Autowired
@@ -32,10 +25,6 @@ public class PlaceService {
             this.placeRepository.save(place);
         return place;}
         else {
-    public void registerPlace(Place place) {
-        if(placeRepository.findPlaceByName(place.getName()) == null)
-            this.placeRepository.save(place);
-        else{
             throw new ConflictException();
 
         }
@@ -61,23 +50,6 @@ public class PlaceService {
 
     public List<Place> findPlaceByCounty(Counties counties) {
         placeList = placeRepository.findAllByCounty(counties);
-    /**
-     * Used to find a list of places thanks to theirs counties in the dataBase
-     * @param counties: enumeration of counties
-     * @return a list of places
-     * @author Dambrine François
-     */
-    public List<Place> findPlaceByCounty(Counties counties){
-        List<Place> placeList = placeRepository.findAllByCounty(counties);
-        return placeList;
-    }
-    /**
-     * Used to find all the places which are in the dataBase
-     * @return a list of all the places
-     * @author Dambrine François
-     */
-    public List<Place> findAllPlace (){
-        List<Place> placeList = (List<Place>) placeRepository.findAll();
         return placeList;
     }
 }
