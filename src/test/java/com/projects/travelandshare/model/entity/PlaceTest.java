ce94f19bbcd82db2ef5fc7870501a71589d37e9b
MarionPradeau <MarionPradeau@users.github.com>
2020-03-20 14:45:33 +0100

    Pull de la branche développement sur la branche test

--- src/test/java/com/projects/travelandshare/model/entity/PlaceTest.java
+++ src/test/java/com/projects/travelandshare/model/entity/PlaceTest.java
@@ -12,11 +12,11 @@ class PlaceTest {
 
     @Test
     void testSetName_setsProperly() throws NoSuchFieldException, IllegalAccessException {
-//given
+    //given
         final Place place = new Place();
-//when
+    //when
         place.setName("Calanques");
-//then
+    //then
         final Field field = place.getClass().getDeclaredField("name");
         field.setAccessible(true);
         assertEquals("Calanques", field.get(place));
@@ -24,14 +24,14 @@ class PlaceTest {
     }
     @Test
     public void testGetName_getsValues() throws NoSuchFieldException, IllegalAccessException {
-//given
+    //given
         final Place place = new Place();
         final Field field = place.getClass().getDeclaredField("name");
         field.setAccessible(true);
         field.set(place, "Paris");
-//when
+    //when
         final String result = place.getName();
-//then
+    //then
         assertEquals("Paris", result);
         System.out.println(place.getName());
     }
